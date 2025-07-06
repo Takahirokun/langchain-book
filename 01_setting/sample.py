17fd4c4cc6106cbcf8f845222d4a21b65f577052
Takahirokun <takkun1789@gmail.com>
2025-07-06 22:40:04 +0900

    fix: fix code of chapter1 for latest version openai api.

--- 01_setting/sample.py
+++ 01_setting/sample.py
@@ -1,14 +1,15 @@
 import json
-import openai  #←OpenAIが用意しているPythonパッケージをインポートする
+from openai import OpenAI
 
-response = openai.ChatCompletion.create(  #←OpenAIのAPIを呼び出すことで、言語モデルを呼び出している
+client = OpenAI()  #←OpenAIが用意しているPythonパッケージをインポートする
+
+response = client.chat.completions.create(#←OpenAIのAPIを呼び出すことで、言語モデルを呼び出している
     model="gpt-3.5-turbo",  #←呼び出す言語モデルの名前
     messages=[
         {
             "role": "user",
             "content": "iPhone8のリリース日を教えて"  #←入力する文章(プロンプト)
         },
-    ]
-)
+    ])
 
-print(json.dumps(response, indent=2, ensure_ascii=False))
+print(response)
